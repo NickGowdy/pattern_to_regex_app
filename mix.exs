3fcf0b4358e59d46d7b882f694a5fa259bfec767
Nick Gowdy <nickgowdy@Nicks-MacBook-Pro.local>
2022-07-11 15:35:10 +0100

    escript doesn't work as I would like it.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,6 @@ defmodule PatternToRegexApp.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      escript: [main_module: Program],
       deps: deps()
     ]
   end
