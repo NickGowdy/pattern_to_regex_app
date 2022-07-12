26e717cf67f288202241fb4c3a62d47480882f9a
Nick Gowdy <nickgowdy@Nicks-MacBook-Pro.local>
2022-07-12 16:29:53 +0100

    Fix broken test.

--- test/program_test.exs
+++ test/program_test.exs
@@ -1,11 +1,23 @@
 defmodule Program.Test do
   use ExUnit.Case, async: true
 
-  test "Can get regex string from module" do
-    assert Program.main('foo %{0} is a %{1S3}')
-           |> IO.read(:all) = "foo [a-zA-Z ]{0,} is a \W*(\w+(\W+|$)){1,4}$"
+  import ExUnit.CaptureIO
 
-    assert Program.main('')
-           |> IO.read(:all)
+  test "Can get regex string from module" do
+    expected = "foo [a-zA-Z ]{0,} is a \\W*(\\w+(\\W+|$)){1,4}$\n"
+    actual = capture_io(fn ->
+      Program.main('foo %{0} is a %{1S3}')
+    end)
+
+    assert expected == actual
+  end
+
+  test "Empty argument returns empty string from program" do
+    expected = "\n"
+    actual = capture_io(fn ->
+      Program.main('')
+    end)
+
+    assert expected == actual
   end
 end
