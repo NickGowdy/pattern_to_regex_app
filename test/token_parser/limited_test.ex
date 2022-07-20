9e9594a6ff1cfbaa159a3b9b1123b5e3e635cac7
Nick Gowdy <nickgowdy@Nicks-MacBook-Pro.local>
2022-07-20 11:13:40 +0100

    Fix limited regex tests.

--- test/token_parser/limited_test.ex
+++ test/token_parser/limited_test.ex
@@ -2,15 +2,23 @@ defmodule TokenParser.Limited.Test do
   use ExUnit.Case, async: true
 
   test "Can verify limited token" do
-    assert = TokenParser.Limited.parse([], 0) == :false
-    assert = TokenParser.Limited.parse([_ | _] = ["{", "1", "}"], 0) == :false
-    assert = TokenParser.Limited.parse([_ | _] = ["1", "}"], 0) == :false
-    assert = TokenParser.Limited.parse([_ | _] = ["}"], 0) == :false
-    assert = TokenParser.Limited.parse([_ | _] = ["%", "{", "-1", "}"], 0) == false
+    assert TokenParser.Limited.parse("") == {:error, {:expected, "%{"}}
+    assert TokenParser.Limited.parse("{1}")  == {:error, {:expected, "%{"}}
+    assert TokenParser.Limited.parse("1}") == {:error, {:expected, "%{"}}
+    assert TokenParser.Limited.parse("}") == {:error, {:expected, "%{"}}
+    assert TokenParser.Limited.parse("%{-1}") == {:error, {:expected, "S or `number`"}}
+    assert TokenParser.Limited.parse("%1G}") == {:error, {:expected, "%{"}}
+    assert TokenParser.Limited.parse("%{1D2}") == {:error, {:expected, "S or `number`"}}
 
-    assert = TokenParser.Limited.parse([_ | _] = ["%", "{", "1", "S", "0", "}"], 0) == :true
-    assert = TokenParser.Limited.parse([_ | _] = ["%", "{", "12", "S", "0", "}"], 0) == :true
-    assert = TokenParser.Limited.parse([_ | _] = ["%", "{", "12", "S", "12", "}"], 0) == :true
-    assert = TokenParser.Limited.parse([_ | _] = ["%", "{", "", "S", "1234", "}"], 0) == :true
+    assert TokenParser.Limited.parse("%{1S1}") == :ok
+    assert TokenParser.Limited.parse("%{1S12}") == :ok
+    assert TokenParser.Limited.parse("%{1S123}") == :ok
+    assert TokenParser.Limited.parse("%{1S1234}") == :ok
+    assert TokenParser.Limited.parse("%{1S12345}") == :ok
+    assert TokenParser.Limited.parse("%{1S123456}") == :ok
+    assert TokenParser.Limited.parse("%{1S1234567}") == :ok
+    assert TokenParser.Limited.parse("%{1S12345678}") == :ok
+    assert TokenParser.Limited.parse("%{1S123456789}") == :ok
+    assert TokenParser.Limited.parse("%{1S1234567890}") == :ok
   end
 end
