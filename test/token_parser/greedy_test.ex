c7789b86e9b0dd660d72020ab85146237c3a5d50
Nick Gowdy <nickgowdy@Nicks-MacBook-Pro.local>
2022-07-20 11:18:27 +0100

    Fix greedy regex tests.

--- test/token_parser/greedy_test.ex
+++ test/token_parser/greedy_test.ex
@@ -2,15 +2,25 @@ defmodule TokenParser.Greedy.Test do
   use ExUnit.Case, async: true
 
   test "Can verify greedy token" do
-    assert = TokenParser.Greedy.parse([], 0) == false
-    assert = TokenParser.Greedy.parse([_ | _] = ["{", "1", "}"], 0) == false
-    assert = TokenParser.Greedy.parse([_ | _] = ["1", "}"], 0) == false
-    assert = TokenParser.Greedy.parse([_ | _] = ["}"], 0) == false
-    assert = TokenParser.Greedy.parse([_ | _] = ["%", "{", "-1", "}"], 0) == false
+    assert TokenParser.Greedy.parse("") == {:error, {:expected, "%{"}}
+    assert TokenParser.Greedy.parse("{1}")  == {:error, {:expected, "%{"}}
+    assert TokenParser.Greedy.parse("1}") == {:error, {:expected, "%{"}}
+    assert TokenParser.Greedy.parse("}") == {:error, {:expected, "%{"}}
+    assert TokenParser.Greedy.parse("%{-1}") ==  {:error, {:expected, "digit or `}`"}}
+    assert TokenParser.Greedy.parse("%1G}") == {:error, {:expected, "%{"}}
+    assert TokenParser.Greedy.parse("%{1D2}") == {:error, {:expected, "digit or `}`"}}
+    assert TokenParser.Greedy.parse("%{1G1}") == {:error, {:expected, "digit or `}`"}}
 
-    assert = TokenParser.Greedy.parse([_ | _] = ["%", "{", "0", "G", "}"], 0) == true
-    assert = TokenParser.Greedy.parse([_ | _] = ["%", "{", "12", "G", "}"], 0) == true
-    assert = TokenParser.Greedy.parse([_ | _] = ["%", "{", "123", "G", "}"], 0) == true
-    assert = TokenParser.Greedy.parse([_ | _] = ["%", "{", "1234", "G", "}"], 0) == true
+    assert TokenParser.Greedy.parse("%{1G}") == :ok
+    assert TokenParser.Greedy.parse("%{12G}") == :ok
+    assert TokenParser.Greedy.parse("%{123G}") == :ok
+    assert TokenParser.Greedy.parse("%{1234G}") == :ok
+    assert TokenParser.Greedy.parse("%{12345G}") == :ok
+    assert TokenParser.Greedy.parse("%{123456G}") == :ok
+    assert TokenParser.Greedy.parse("%{1234567G}") == :ok
+    assert TokenParser.Greedy.parse("%{12345678G}") == :ok
+    assert TokenParser.Greedy.parse("%{123456789G}") == :ok
+    assert TokenParser.Greedy.parse("%{1234567890G}") == :ok
+
   end
 end
