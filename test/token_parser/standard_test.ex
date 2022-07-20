f07a14d98438838c47305887921db24248f4ee9d
Nick Gowdy <nickgowdy@Nicks-MacBook-Pro.local>
2022-07-20 11:02:18 +0100

    Fix standard regex tests.

--- test/token_parser/standard_test.ex
+++ test/token_parser/standard_test.ex
@@ -2,20 +2,21 @@ defmodule TokenParser.Standard.Test do
   use ExUnit.Case, async: true
 
   test "Can verify standard token" do
-    assert = TokenParser.Simple.parse([], 0) == false
-    assert = TokenParser.Simple.parse([_ | _] = ["{", "1", "}"], 0) == false
-    assert = TokenParser.Simple.parse([_ | _] = ["1", "}"], 0) == false
-    assert = TokenParser.Simple.parse([_ | _] = ["}"], 0) == false
-    assert = TokenParser.Simple.parse([_ | _] = ["%", "{", "-1", "}"], 0) == false
+    assert TokenParser.Simple.parse("") == {:error, {:expected, "%{"}}
+    assert TokenParser.Simple.parse("{1}") == {:error, {:expected, "%{"}}
+    assert = TokenParser.Simple.parse("1}") == {:error, {:expected, "%{"}}
+    assert = TokenParser.Simple.parse("}")  == {:error, {:expected, "%{"}}
+    assert = TokenParser.Simple.parse("%{-1}") == {:error, {:expected, "%{"}}
 
-    assert = TokenParser.Simple.parse([_ | _] = ["%", "{", "1", "}"], 0) == true
-    assert = TokenParser.Simple.parse([_ | _] = ["%", "{", "12", "}"], 0) == true
-    assert = TokenParser.Simple.parse([_ | _] = ["%", "{", "123", "}"], 0) == true
-    assert = TokenParser.Simple.parse([_ | _] = ["%", "{", "1234", "}"], 0) == true
-    assert = TokenParser.Simple.parse([_ | _] = ["%", "{", "12345", "}"], 0) == true
-    assert = TokenParser.Simple.parse([_ | _] = ["%", "{", "123456", "}"], 0) == true
-    assert = TokenParser.Simple.parse([_ | _] = ["%", "{", "1234567", "}"], 0) == true
-    assert = TokenParser.Simple.parse([_ | _] = ["%", "{", "12345678", "}"], 0) == true
-    assert = TokenParser.Simple.parse([_ | _] = ["%", "{", "123456789", "}"], 0) == true
+    assert = TokenParser.Simple.parse("%{1}") == :ok
+    assert = TokenParser.Simple.parse("%{12}") == :ok
+    assert = TokenParser.Simple.parse("%{123}") == :ok
+    assert = TokenParser.Simple.parse("%{1234}") == :ok
+    assert = TokenParser.Simple.parse("%{12345}") == :ok
+    assert = TokenParser.Simple.parse("%{123456}") == :ok
+    assert = TokenParser.Simple.parse("%{1234567}") == :ok
+    assert = TokenParser.Simple.parse("%{12345678}") == :ok
+    assert = TokenParser.Simple.parse("%{123456789}") == :ok
+    assert = TokenParser.Simple.parse("%{1234567890}") == :ok
   end
 end
